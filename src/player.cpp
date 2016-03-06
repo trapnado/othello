2b0cd60e10ac1e2ee7bba2d64b0024317bf10500
rsonka <rfsonka@gmail.com>
2016-03-05 17:12:20 -0800

    Gave player a Side and a Board. Created folders bin, obj, and src and rewrote the MAKEFILE to work with them as expected (I think) to make it so compiling didn't FLOOD the main folder with files.

--- src/player.cpp
+++ src/player.cpp
@@ -8,7 +8,8 @@
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
-
+    mySide = side;
+    board = Board(); 
     /* 
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
